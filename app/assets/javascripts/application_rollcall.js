a7b402896dab61a618b7008ba2650416f8a8efe6
Thongchat Teparuk <thongchat.tr@gmail.com>
2019-05-31 17:26:21 +0700

    change_path_image

--- app/assets/javascripts/application_rollcall.js
+++ app/assets/javascripts/application_rollcall.js
@@ -38,6 +38,7 @@
 //= require rollcall/angular/app.assets
 
 //= require bootstrap-table
+//= require bootstrap-select/js/bootstrap-select
 //= ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min
 //= require rails.validations
 //= require rails.validations.simple_form
