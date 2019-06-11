b475ea130a42a7b1da863500fb68e3d9d8eea26b
Songyot Jaichai <songyot_jaichai@hotmail.com>
2019-06-11 13:33:55 +0700

    fix code

--- app/mailers/school_mailer.rb
+++ app/mailers/school_mailer.rb
@@ -1,12 +1,8 @@
 class SchoolMailer < ApplicationMailer
     default from: 'notifications@example.com'
-    
-    def school_notification(school)
-        @school = school
-        @email = "#{@school.users.first.email}"
-        mail(to: @email, subject: 'test')
-    
-    end
-
-
+  def school_notification(school)
+    @school = school
+    @email = "#{@school.users.first.email}"
+    mail(to: @email, subject: 'test')
+  end
 end
