1b886c6b904ff60819f4da41d12ca38cf704b38a
Panmai Tanu <panmai@bananacoding.com>
2019-06-21 12:53:36 +0700

    send mail invite to employee

--- app/mailers/employee_mailer.rb
+++ app/mailers/employee_mailer.rb
@@ -1,6 +1,7 @@
 class EmployeeMailer < ApplicationMailer
-	def send_employee_invite(employee)
+	def send_employee_invite(employee, token)
 		@employee = employee
+		@token = token
 		mail(to: employee.email, subject: "ยินดีต้อนรับสู่ Bananacoding")
 	end	
 
