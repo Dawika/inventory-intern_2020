caec7c76f7e31dee361fd4373842d8f095f29bdb
Songyot Jaichai <songyot_jaichai@hotmail.com>
2019-07-18 17:04:51 +0700

    fiexd classroom in school

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20190718032543) do
-=======
 ActiveRecord::Schema.define(version: 20190718085305) do
->>>>>>> edit classroom in school
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
