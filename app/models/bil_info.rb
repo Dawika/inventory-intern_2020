c4a3d79f4640ef1c0537bf010beae48a50fb8625
Songyot Jaichai <songyot_jaichai@hotmail.com>
2019-08-06 10:42:38 +0700

    design db

--- app/models/bil_info.rb
+++ app/models/bil_info.rb
@@ -1,12 +1,6 @@
 class BilInfo < ApplicationRecord
     has_one :school
-    attr_accessor :check
-    attr_accessor :payment_method
-    attr_accessor :cardholder_name
-    attr_accessor :card_number
-    attr_accessor :exp_month
-    attr_accessor :exp_year
-    attr_accessor :cvv
+    attr_accessor :check, :payment_method, :cardholder_name, :card_number, :exp_month, :exp_year, :cvv
     validates :payment_method, :cardholder_name, :card_number, :exp_month, :exp_year, :cvv, presence: true, if: :transfer_money
     validates :name, :address, :district, :province, :zip_code, :phone, :tax_id, :branch, presence: true
 
