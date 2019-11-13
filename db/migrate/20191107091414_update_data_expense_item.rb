d0ea4367efac012593119b5e4f72c26eedc170ef
Panmai Tanu <panmai@cmu.ac.th>
2019-11-13 14:49:51 +0700

    fix migrate expense tag item data

---
+++ db/migrate/20191107091414_update_data_expense_item.rb
@@ -0,0 +1,19 @@
+class UpdateDataExpenseItem < ActiveRecord::Migration[5.0]
+  def change
+    tag_tree = SiteConfig.get_cache.expense_tag_tree_hash
+    min_lv = nil
+    tag_tree.each do |t|
+      min_lv = t and next if min_lv.blank?
+      if t[:lv] < min_lv[:lv]
+        min_lv = t
+      else
+        expense_item_ids = ActiveRecord::Base.connection.exec_query('select id from expense_tag_items;').rows.flatten.uniq
+        ExpenseItem.where(id: expense_item_ids).update(expense_tag_id: min_lv[:id])
+        min_lv = t
+      end
+    end
+
+    expense_item_ids = ActiveRecord::Base.connection.exec_query('select id from expense_tag_items;').rows.flatten.uniq
+    ExpenseItem.where(id: expense_item_ids).update(expense_tag_id: min_lv[:id])
+  end
+end
