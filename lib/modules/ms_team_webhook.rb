6632ea07f48830ba12bd5e3d50ce4e3d75216bfe
Kraiwut Arayangkura <kraiwuthi.a@gmail.com>
2020-01-09 13:10:01 +0700

    send notification on production only

--- lib/modules/ms_team_webhook.rb
+++ lib/modules/ms_team_webhook.rb
@@ -3,7 +3,7 @@ require 'net/http'
 
 module MsTeamWebhook
   def send_webhook(text, title = nil)
-    if Figaro.env.TEAM_CONNECTOR_WEBHOOK_URL?
+    if Figaro.env.TEAM_CONNECTOR_WEBHOOK_URL? && Rails.env.production?
       teams = Teams.new(Figaro.env.TEAM_CONNECTOR_WEBHOOK_URL)
       teams.post(text, title: title)
     end
