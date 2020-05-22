56dfd7df6d798ed38a1bb2083969b53dabaedb94
songyot jaichai <songyot@bananacoding.com>
2020-05-22 11:29:33 +0700

    fix code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   # mail config
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'www.bananahacking.com' }
+  config.action_mailer.default_url_options = { host: Figaro.env.DEFAULT_URL_MAILER_ON_PRODUCTION }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:                'smtp.sendgrid.net',
@@ -97,14 +97,14 @@ Rails.application.configure do
     domain:                 'heroku.com',
     enable_starttls_auto:   true
   }
-  config.action_mailer.asset_host = 'https://www.bananahacking.com'
+  config.action_mailer.asset_host = Figaro.env.DEFAULT_URL_MAILER_ON_PRODUCTION
   config.assets.comple = false
   config.assets.precompile += ['pdf.css']
 
   Rails.application.config.middleware.use ExceptionNotification::Rack,
     :email => {
       :email_prefix => "[Exception] ",
-      :sender_address => %{"notifier" <notifier@bananahacking.com>},
+      :sender_address => %{"notifier" <notifier@#{Figaro.env.EXCEPTIONNOTIFICATION}>},
       :exception_recipients => %w{exceptions@bananahacking.com}
     }
 
