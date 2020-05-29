f19afeb86bfafd35b900c2448225ec47b0577ce8
Nutchaporn <nutchaporn@Nutchaporns-MacBook-Air.local>
2020-05-29 10:48:53 +0700

    แก้หน้าเพิ่มนักเรียนที่ยังแสดงข้อมูลห้องเรียนออกมาไม่ถูกต้อง

--- app/assets/javascripts/custom_bootstrap_table.js
+++ app/assets/javascripts/custom_bootstrap_table.js
@@ -53,7 +53,7 @@ function exportTable(tableName) {
   var url = tableName + '.json?order=' + window.paramOder + '&sort=' + window.paramSort + '&search=' + $("input#search").val() + '&grade_select=' + $("select#grade_select").val() + '&class_select=' + $("select#class_select").val()
 
   $table = $('#tableForExport')
-  $.get(url, function(data) {
+  $.get(url, function (data) {
     $table.bootstrapTable('load', data.rows)
     $table.tableExport({ type: 'excel' });
   });
@@ -66,35 +66,35 @@ function imgTag(value, row, index) {
   return '<div class="img-bg bg-light-gray circle"><i class="fa fa-user icon-default-img" aria-hidden="true"></i></div>'
 }
 
-function selectionStudentFormatter(value, row, index){
+function selectionStudentFormatter(value, row, index) {
   var html =
-  '<span class="dropdown float-right cursor-pointer">' +
+    '<span class="dropdown float-right cursor-pointer">' +
     '<span div data-toggle="dropdown" id="options' + row.id + '">' +
-      "<a class='color-blue-link'>"+I18n.t("choice")+"<i class='fa fa-angle-down ml-5'></i></a>" +
+    "<a class='color-blue-link'>" + I18n.t("choice") + "<i class='fa fa-angle-down ml-5'></i></a>" +
     '</span>' +
     '<ul class="dropdown-menu dropdown-menu-right" aria-labelledby="options' + row.id + '">' +
-      '<li>' +
-        "<a href='/students/" + row.id + "/edit'>" +
-          "<i class='fa fa-pencil-square-o fa-fw' aria-hidden='true'></i>&nbsp;"+I18n.t("edit")+
-        '</a>' +
-      '</li>' +
-      '<li>' +
-        "<a onclick='openResignStudentModal(" + row.id + ")'>" +
-          "<i class='fa fa-share-square-o color-orange fa-fw' aria-hidden='true'></i>&nbsp;"+I18n.t("resign")+
-        '</a>' +
-      '</li>' +
-      '<li>' +
-        "<a onclick='openGraduateStudentModal(" + row.id + ")'>" +
-          "<i class='fa fa-graduation-cap color-green fa-fw' aria-hidden='true'></i>&nbsp;"+I18n.t("gradulated")+
-        '</a>' +
-      '</li>' +
-      '<li>' +
-        "<a onclick='openDeletedStudentModal(" + row.id + ")'>" +
-          "<i class='fa fa-trash color-red fa-fw' aria-hidden='true'></i>&nbsp;"+I18n.t("delete")+
-        '</a>' +
-      '</li>' +
+    '<li>' +
+    "<a href='/students/" + row.id + "/edit'>" +
+    "<i class='fa fa-pencil-square-o fa-fw' aria-hidden='true'></i>&nbsp;" + I18n.t("edit") +
+    '</a>' +
+    '</li>' +
+    '<li>' +
+    "<a onclick='openResignStudentModal(" + row.id + ")'>" +
+    "<i class='fa fa-share-square-o color-orange fa-fw' aria-hidden='true'></i>&nbsp;" + I18n.t("resign") +
+    '</a>' +
+    '</li>' +
+    '<li>' +
+    "<a onclick='openGraduateStudentModal(" + row.id + ")'>" +
+    "<i class='fa fa-graduation-cap color-green fa-fw' aria-hidden='true'></i>&nbsp;" + I18n.t("gradulated") +
+    '</a>' +
+    '</li>' +
+    '<li>' +
+    "<a onclick='openDeletedStudentModal(" + row.id + ")'>" +
+    "<i class='fa fa-trash color-red fa-fw' aria-hidden='true'></i>&nbsp;" + I18n.t("delete") +
+    '</a>' +
+    '</li>' +
     '</ul>' +
-  '</span>';
+    '</span>';
   return html
 }
 
@@ -102,7 +102,7 @@ function linkToStudentEditFormatter(value, row, index) {
   return "<a class='color-blue-link' href='/students/" + row.id + "/edit'>" + row.full_name + "</a>"
 }
 
-function openDeletedStudentModal(id){
+function openDeletedStudentModal(id) {
   $('#warningModal #modal-title').html(I18n.t("del_student"))
   $('#warningModal #actionModalForm').prop("action", "/students/" + id)
   $('#warningModal #actionModalForm').append('<input type="hidden" name="_method" value="delete">')
@@ -110,14 +110,14 @@ function openDeletedStudentModal(id){
   $('#warningModal').modal()
 }
 
-function openResignStudentModal(id){
+function openResignStudentModal(id) {
   $('#warningModal #modal-title').html(I18n.t("change_status_student_resign"))
   $('#warningModal #actionModalForm').prop("action", "/students/" + id + "/resign")
   $('#warningModal #actionModalForm').prop("method", "post")
   $('#warningModal').modal()
 }
 
-function openGraduateStudentModal(id){
+function openGraduateStudentModal(id) {
   $('#warningModal #modal-title').html(I18n.t("change_status_student_graduated"))
   $('#warningModal #actionModalForm').prop("action", "/students/" + id + "/graduate")
   $('#warningModal #actionModalForm').prop("method", "post")
@@ -130,19 +130,19 @@ function selectionParentFormatter(value, row, index) {
     var html =
       '<span class="dropdown float-right cursor-pointer cursor-pointer">' +
       '<span div data-toggle="dropdown" id="options' + id + '">' +
-        "<a class='color-blue-link'>"+I18n.t("choice")+"<i class='fa fa-angle-down ml-5'></i></a>" +
+      "<a class='color-blue-link'>" + I18n.t("choice") + "<i class='fa fa-angle-down ml-5'></i></a>" +
       '</span>' +
       '<ul class="dropdown-menu dropdown-menu-right" aria-labelledby="options' + id + '">' +
-        '<li>' +
-          "<a href='/parents/" + id + "/edit'>" +
-            "<i class='fa fa-pencil-square-o fa-fw' aria-hidden='true'></i>&nbsp;"+I18n.t("edit")+
-          '</a>' +
-        '</li>' +
-        '<li>' +
-          "<a onclick='openDeletedParentModal(" + id + ")'>" +
-            "<i class='fa fa-trash color-red fa-fw' aria-hidden='true'></i>&nbsp;"+I18n.t("delete")+
-          '</a>' +
-        '</li>' +
+      '<li>' +
+      "<a href='/parents/" + id + "/edit'>" +
+      "<i class='fa fa-pencil-square-o fa-fw' aria-hidden='true'></i>&nbsp;" + I18n.t("edit") +
+      '</a>' +
+      '</li>' +
+      '<li>' +
+      "<a onclick='openDeletedParentModal(" + id + ")'>" +
+      "<i class='fa fa-trash color-red fa-fw' aria-hidden='true'></i>&nbsp;" + I18n.t("delete") +
+      '</a>' +
+      '</li>' +
       '</ul>' +
       '</span>';
     return html
@@ -160,7 +160,7 @@ function linkToParentEditFormatter(value, row, index) {
 
 }
 
-function openDeletedParentModal(id){
+function openDeletedParentModal(id) {
   $('#warningModal #modal-title').html(I18n.t("del_parent"))
   $('#warningModal #actionModalForm').prop("action", "/parents/" + id)
   $('#warningModal #actionModalForm').append('<input type="hidden" name="_method" value="delete">')
