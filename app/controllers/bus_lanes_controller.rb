005d773f0c56b5c2616cd409d188ea762a88c6a3
songyot jaichai <songyot@bananacoding.com>
2020-07-08 16:21:43 +0700

    edit bus lane

--- app/controllers/bus_lanes_controller.rb
+++ app/controllers/bus_lanes_controller.rb
@@ -1,12 +1,12 @@
 class BusLanesController < ApplicationController
 
   def index
-    bus = BusLane.where(school_id: current_user.school_id)
+    bus = BusLane.where(school_id: current_user.school_id).order(id: 'desc')
+    total = bus.size
     bus = bus.offset(params[:offset]).limit(params[:limit])
-
     render json: {
       rows: bus.as_json('index'),
-      total: 11
+      total: total
     }, status: :ok
   end
 
@@ -29,19 +29,60 @@ class BusLanesController < ApplicationController
     }
   end
 
+  def get_buslane
+    bus_lane = BusLane.find(params[:bus_lane])
+    students = []
+    bus_lane.students.includes(:grade, :classroom, school: [:school_settings]).each do |student| 
+      classroom = student.classroom&.name.present? ? "#{student.grade&.name}/#{student.classroom&.name}" : "#{student.grade&.name}"
+      students << {
+        id: student.id,
+        full_name: student.full_name,
+        classroom: classroom
+    }
+    end
+    render json: {
+      name: bus_lane.name,
+      bus_lane: bus_lane.bus,
+      employee: bus_lane.employees.first,
+      note: bus_lane.note,
+      students: students
+    }
+  end
+
   def create
     BusLane.transaction do
-      buslane = BusLane.create(params_buslane)
-      params[:bus_lane][:student_id].each_with_index do |id, index|
-        buslane.students_bus_lanes.create(student_id:  params[:bus_lane][:student_id][index][:id])
-      end
-      buslane.employees_bus_lanes.create(employee_id: params[:bus_lane][:employee_id])
+      @buslane = BusLane.create(params_buslane)
+      create_student
+      @buslane.employees_bus_lanes.create(employee_id: params[:bus_lane][:employee_id])
     end
+    render json: {success: true}, status: :ok
+  end
+
+  def update
+    ActiveRecord::Base.transaction do
+      @buslane = BusLane.find(params[:id])
+      @buslane.update(params_buslane)
+      @buslane.students_bus_lanes.delete_all
+      create_student
+      @buslane.employees_bus_lanes.first.update(employee_id: params[:bus_lane][:employee_id])
+    end
+    render json: {success: true}, status: :ok
+  end
+
+  def destroy
+    buslane = BusLane.find(params[:id])
+    buslane.destroy
     render json: {success: true}, status: :ok
   end
 
   def params_buslane
     params.require(:bus_lane).permit(:name, :bus_id, :note).merge(school_id: current_user.school_id)
+  end
+
+  def create_student
+    params[:bus_lane][:student_id].each_with_index do |id, index|
+      @buslane.students_bus_lanes.create(student_id:  params[:bus_lane][:student_id][index][:id])
+    end
   end
 
 end
--- app/models/bus_lane.rb
+++ app/models/bus_lane.rb
@@ -1,13 +1,14 @@
 class BusLane < ApplicationRecord
   belongs_to :bus
-  has_many :students_bus_lanes
-  has_many :employees_bus_lanes
+  has_many :students_bus_lanes, dependent: :destroy
+  has_many :employees_bus_lanes, dependent: :destroy
   has_and_belongs_to_many :employees, join_table: "employees_bus_lanes"
   has_and_belongs_to_many :students, join_table: "students_bus_lanes"
 
   def as_json(options = {})
     if options['index']
       {
+        id: self.id,
         name: self.name,
         bus: self.bus.license_plate,
         employee: self.employees.first&.full_name,
