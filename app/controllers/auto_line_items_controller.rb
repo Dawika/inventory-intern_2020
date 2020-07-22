1a27068b195c066f8cbd99512325914b5d217ad1
songyot jaichai <songyot@bananacoding.com>
2020-07-22 14:17:18 +0700

    manage line item

--- app/controllers/auto_line_items_controller.rb
+++ app/controllers/auto_line_items_controller.rb
@@ -1,7 +1,31 @@
 class AutoLineItemsController < ApplicationController
-  
+  before_action :find_autolineitem, only: [:update, :destroy]
+
   def index
-    item = AutoLineItem.where(school_id: current_user.school_id)
+    item = AutoLineItem.where(school_id: current_user.school_id).order(id: 'desc')
     render json: item, status: :ok
+  end
+
+  def create
+    AutoLineItem.create(auto_line_item_params)
+    index
+  end
+
+  def update
+    @item.update(auto_line_item_params)
+    index
+  end
+
+  def destroy
+    @item.destroy
+    index
+  end
+
+  def find_autolineitem
+    @item = AutoLineItem.find(params[:id])
+  end
+
+  def auto_line_item_params
+    params.require(:auto_line_item).permit(:name, :price).merge(school_id: current_user.school_id)
   end
 end
